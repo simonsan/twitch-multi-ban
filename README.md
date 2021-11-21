867b0a052d5426f218232fa530b081ff21f7b872
Simon San <14062932+simonsan@users.noreply.github.com>
2021-11-21 15:18:47 +0100

    Implement banning from Banlist

--- README.md
+++ README.md
@@ -1,25 +1,38 @@
-# Twitch Send
+# Multi Ban for Twitch
 
-Command Line Tool to send messages to your twitch chat
-
-## Prerequesites
-
-- cargo
+Command line tool to ban alt accounts on Twitch
 
 ## Setup twitch authentication
 
-You will need to set the following environment variables
-- TWITCH_SEND_TOKEN can be generated at [twitchapps.com/tmi](https://twitchapps.com/tmi).
-- TWITCH_SEND_NAME is your twitch name
-- TWITCH_SEND_CHANNEL the twitch channel to join
+You will need to set the following environment variables:
 
-## Install the binary
+- TWITCH_SEND_TOKEN: can be generated at [twitchapps.com/tmi](https://twitchapps.com/tmi).
+- TWITCH_SEND_NAME: is your twitch name
+- TWITCH_SEND_CHANNEL: the twitch channel to join
 
-```bash
-cargo install --force twitch-send
+## Setup ban list
+
+Ban list schema:
+
+```yaml
+--- # entry
+- date: 11/20/2031
+  comment: Update to username
+  content:
+    - username123
+    - username456
 ```
 
+You will need to set the following environment variables:
+
+- TWITCH_BAN_LIST: URL to RAW file of account list
+
+## Download
+
+You can download binary files here: <https://github.com/simonsan/twitch-multi-ban/releases>
+
 ## Run
+
 ```bash
-twitch-send Hello Twitch!
+twitch-multi-ban <reason>
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
+use serde::{Deserialize, Serialize};
 use twitch_irc::login::{CredentialsPair, StaticLoginCredentials};
-use twitch_irc::ClientConfig;
-use twitch_irc::TCPTransport;
-use twitch_irc::TwitchIRCClient;
+use twitch_irc::{ClientConfig, SecureTCPTransport, TwitchIRCClient};
 
 fn channel_to_join() -> Result<String, Box<dyn std::error::Error>> {
     let channel = get_env_var("TWITCH_SEND_CHANNEL")?;
@@ -13,13 +12,25 @@ fn get_env_var(key: &str) -> Result<String, Box<dyn std::error::Error>> {
     Ok(my_var)
 }
 
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct UsernameUpdate {
+    date: String,
+    comment: String,
+    pub content: Vec<String>,
+}
+
 #[tokio::main]
 async fn main() {
+    let dl = get_env_var("TWITCH_BAN_LIST").unwrap();
     let twitch_name = get_env_var("TWITCH_SEND_NAME").unwrap();
     let twitch_token = get_env_var("TWITCH_SEND_TOKEN")
         .unwrap()
         .replacen("oauth:", "", 1);
     let channel_to_join = channel_to_join().unwrap();
+
+    let alt_dl: String = ureq::get(&dl).call().unwrap().into_string().unwrap();
+
+    let username_list: Vec<UsernameUpdate> = serde_yaml::from_str(&alt_dl).unwrap();
 
     // default configuration is to join chat as anonymous.
     let config = ClientConfig {
@@ -32,12 +43,39 @@ async fn main() {
         ..ClientConfig::default()
     };
 
-    let (mut _incoming_messages, client) =
-        TwitchIRCClient::<TCPTransport, StaticLoginCredentials>::new(config);
+    let (mut incoming_messages, client) =
+        TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
+
+    let cloned_client = client.clone();
+
+    let join_handle = tokio::spawn(async move {
+        while let Some(message) = incoming_messages.recv().await {
+            println!("Received message: {:?}", message);
+        }
+    });
 
     // join a channel
-    client.join(channel_to_join.clone());
-    let msg = std::env::args().skip(1).collect::<Vec<_>>().join(" ");
+    cloned_client.join(channel_to_join.clone());
 
-    client.say(twitch_name, msg).await.unwrap();
+    let usernames = username_list
+        .iter()
+        .map(|x| x.content.to_owned())
+        .flatten()
+        .collect::<Vec<_>>();
+
+    let reason = std::env::args().skip(1).collect::<Vec<_>>().join(" ");
+
+    for username in usernames {
+        let clean_username = username.replacen('"', "", 2);
+        println!("Sending ban command for {:?} ...", clean_username);
+        cloned_client
+            .ban(twitch_name.clone(), &clean_username, Some(&reason))
+            .await
+            .unwrap();
+    }
+
+    join_handle.await.unwrap();
+
+    drop(client);
+    drop(cloned_client);
 }
